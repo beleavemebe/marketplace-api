02dd610a70dbf12e068f44e486f2e8d80d13638f
beleavemebe <cborzyy@gmail.com>
2022-03-24 22:29:00 +0300

    Fix wrong app package

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 group = "com.narcissus"
 version = "0.0.1"
 application {
-    mainClass.set("com.narcissus.ApplicationKt")
+    mainClass.set("com.narcissus.marketplace.api.ApplicationKt")
 }
 
 repositories {
