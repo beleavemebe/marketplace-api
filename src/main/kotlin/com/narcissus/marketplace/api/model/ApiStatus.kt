07ae3ad25902c60c83ca4a26fbec942e084a442a
beleavemebe <cborzyy@gmail.com>
2022-03-31 16:03:18 +0300

    Change serial name of `ApiStatus.isAvailable` to snake case

--- src/main/kotlin/com/narcissus/marketplace/api/model/ApiStatus.kt
+++ src/main/kotlin/com/narcissus/marketplace/api/model/ApiStatus.kt
@@ -1,8 +1,10 @@
 package com.narcissus.marketplace.api.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiStatus(
+    @SerialName("is_available")
     val isAvailable: Boolean
 )
